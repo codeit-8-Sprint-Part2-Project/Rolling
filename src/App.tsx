25d1b8f35dee52986ff67bef977924711ba6304c
KJongHwa <dblls0724@naver.com>
2024-07-12 12:29:57 +0900

    Feat : Pages 폴더 생성, HomePage 초기값 지정, React-Router-dom 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
