99ffe585c60f812e5f57924b438df22318012ad9
김영주 <breadjoo1014@naver.com>
2024-07-12 20:50:42 +0900

    Feat(App.tsx): 리액트 라우터 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import './App.css';
+import * as React from "react";
+import ThemeProvider from "./pages/postTheme/core/ThemeProvider";
+import PostTheme from "./pages/postTheme/PostTheme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <ThemeProvider>
+              <PostTheme />
+            </ThemeProvider>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
