d3eb1cdf3f1023dc702545b6d041d33561358a59
KJongHwa <dblls0724@naver.com>
2024-07-12 13:50:52 +0900

    Feat : Nav.tsx 파일 1차 완성

--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom";
 
 const Nav: React.FC = () => {
   return (
-    <div>
-      <Link to="/">
-        <div className="flex flex-row align-items-center">
-          <img
-            className="mr-2"
-            src="/assets/icons/IconColor.png"
-            alt="Rolling 로고"
-            width="27.82"
-          />
-          <p className="font-poppins font-bold text-xl text-gray-800">
-            Rolling
-          </p>
-        </div>
-      </Link>
+    <div className="border-b border-solid border-gray-300 h-16 flex items-center">
+      <div className="container mx-auto max-w-[1200px]">
+        <Link to="/">
+          <div className="flex flex-row items-center">
+            <img
+              className="mr-2"
+              src="/assets/icons/IconColor.png"
+              alt="Rolling 로고"
+              width="27.82"
+            />
+            <p className="font-poppins font-bold text-xl text-gray-800">
+              Rolling
+            </p>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
