3ff7c6435a90f2169df76908fe71c845dc022bde
김영주 <breadjoo1014@naver.com>
2024-07-15 01:13:50 +0900

    Chore: 테일윈드 CSS 커스텀 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        beige: "#FFE2AD",
+      },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
         pretendard: ["Pretendard", "sans-serif"],
