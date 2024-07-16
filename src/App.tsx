c3747ea8532766485a8634985e5bf2bb99ce2cd6
DasomKim <ekt0521@naver.com>
2024-07-16 20:00:43 +0900

    Remove: 임시로 적용했던 헤더, 포스트페이지 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
 import ListPage from "./pages/ListPage/ListPage";
-import PostPage from "./pages/PostPage/PostPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<HomePage />} />
+        <Route />
         <Route path="list" element={<ListPage />} />
-        <Route path="post" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../../components/Layout/Header";
 import BestRecipient from "./components/BestRecipient";
 import AllRecipient from "./components/AllRecipient";
 
 const ListPage: React.FC = () => {
   return (
     <div>
-      <Header />
       <BestRecipient />
       <AllRecipient />
     </div>
