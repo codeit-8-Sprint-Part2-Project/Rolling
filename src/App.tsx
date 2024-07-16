aacce58d7e77185400218f3bd1292382ed237aa8
unknown <jingmsi014@gmail.com>
2024-07-16 23:24:12 +0900

    chore: gitignore 수정, App.tsx 원상복구

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import './App.css';
-import PostEditPage from './pages/PostPage/PostEditPage';
-import PostPage from './pages/PostPage/PostPage';
 
 function App() {
   return (
-    <PostEditPage />
+    <div>
+    </div>
   );
 };
 
