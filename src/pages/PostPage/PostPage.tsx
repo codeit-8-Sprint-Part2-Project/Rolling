6aa96de502d5ccfc6488c061cd03d90d6b8431c0
KJongHwa <dblls0724@naver.com>
2024-07-16 16:20:09 +0900

    Remove : postpage 관련 삭제

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Nav from "../../components/Layout/Nav";
 import PostNav from "./components/Layout/PostNav";
-import Posts from "../../components/post/Posts";
 
 const PostPage: React.FC = () => {
   return (
     <div>
       <Nav />
       <PostNav />
-      <Posts id="8316" />
     </div>
   );
 };
