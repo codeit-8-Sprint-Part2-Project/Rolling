4eaf649b8598dec207dbb2de39d85e02bee91a99
SealBros <hoon980923@gmail.com>
2024-07-18 00:55:03 +0900

    feat : inputProfileSection.tsx 작업

--- src/pages/MessagePage/components/InputProfileImageSection.tsx
+++ src/pages/MessagePage/components/InputProfileImageSection.tsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from 'react';
 import useProfileImages from "../hooks/useProfileImage";
 
-const MyComponent = () => {
+const inputProfileSection: React.FC = () => {
   const profileImages = useProfileImages();
+  const [selectedImageUrl, setSelectedImageUrl] = useState<string | null>(null);
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      setSelectedImageUrl(URL.createObjectURL(event.target.files[0]));
+    } else {
+      setSelectedImageUrl(null);
+    }
+  };
+  // 사용자 이미지 선택 확인 함수 
+  const getDisplayImageUrl = (): string => {
+    if (selectedImageUrl) {
+      return selectedImageUrl;
+    } else if (profileImages && profileImages.imageUrls.length > 0) {
+      return profileImages.imageUrls[0];
+    } else {
+      return 'Rolling/src/assets/images/defaultProfileImage.png';
+    }
+  };
 
   if (!profileImages) {
     return <div>Loading...</div>;
   }
-
   return (
-    <div>
-      {profileImages.imageUrls.map((imageUrl, index) => (
-        <img key={index} src={imageUrl} alt={`Profile Image ${index}`} />
-      ))}
-    </div>
+    <section className="flex flex-col gap-12 w-full">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-1">
+          <p className="font-bold text-2xl">프로필 이미지</p>
+          <div>
+            <img src={getDisplayImageUrl()} alt="프로필 이미지" />
+            <input type="file" onChange={handleImageUpload} />
+            <p className="text-gray-500">프로필 이미지를 선택해주세요!</p>
+          </div>
+        </div>
+        <div className="flex flex-wrap gap-4">
+          {profileImages.imageUrls.map((imageUrl, index) => (
+            <button
+              key={index}
+              className="bg-gray-200 rounded-md px-4 py-2 hover:bg-gray-300"
+              onClick={() => setSelectedImageUrl(imageUrl)}
+            >
+              <img src={imageUrl} alt={`Image ${index}`} className="w-20 h-20 object-cover" />
+            </button>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
-export default MyComponent;
+export default inputProfileSection;
