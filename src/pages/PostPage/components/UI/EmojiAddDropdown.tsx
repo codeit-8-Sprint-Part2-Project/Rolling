e8367e4fb4c90a6a909754b85af099b4ed11f8ab
KJongHwa <dblls0724@naver.com>
2024-07-17 11:51:14 +0900

    Feat : Emoji 라이브러리 한글화 작업

--- src/pages/PostPage/components/UI/EmojiAddDropdown.tsx
+++ src/pages/PostPage/components/UI/EmojiAddDropdown.tsx
@@ -4,6 +4,7 @@ import { getByPostId } from "../../api/getByPostId";
 import { MessageRetrieve } from "../../../../DTO/message/MessageRetrieve";
 import { ReactionRetrieve } from "../../../../DTO/reaction/ReactionRetrieve";
 import EmojiPicker from "emoji-picker-react";
+import { Categories } from "emoji-picker-react";
 import IconAdd from "../../assets/icons/IconEmojiAdd.png";
 
 interface Recipient {
@@ -47,6 +48,50 @@ function EmojiAddDropdown() {
     return <p>총 이모티콘을 불러오지 못했습니다.</p>;
   }
 
+  interface CustomNames {
+    category: Categories;
+    name: string;
+  }
+
+  const customNames: CustomNames[] = [
+    {
+      category: Categories.SUGGESTED,
+      name: "자주 사용하는 이모티콘",
+    },
+    {
+      category: Categories.SMILEYS_PEOPLE,
+      name: "스마일 이모티콘",
+    },
+    {
+      category: Categories.ANIMALS_NATURE,
+      name: "동물 및 자연 이모티콘",
+    },
+    {
+      category: Categories.FOOD_DRINK,
+      name: "음식 및 음료 이모티콘",
+    },
+    {
+      category: Categories.TRAVEL_PLACES,
+      name: "여행 및 장소 이모티콘",
+    },
+    {
+      category: Categories.ACTIVITIES,
+      name: "활동 이모티콘",
+    },
+    {
+      category: Categories.OBJECTS,
+      name: "물건 이모티콘",
+    },
+    {
+      category: Categories.SYMBOLS,
+      name: "기호 이모티콘",
+    },
+    {
+      category: Categories.FLAGS,
+      name: "국기 이모티콘",
+    },
+  ];
+
   return (
     <>
       <button
@@ -59,7 +104,7 @@ function EmojiAddDropdown() {
 
       {isDropdownVisible && (
         <div className="border border-[#cccccc] shadow-custom absolute top-[40px] left-[-70px]">
-          <EmojiPicker />
+          <EmojiPicker searchPlaceholder="검색" categories={customNames} />
         </div>
       )}
     </>
