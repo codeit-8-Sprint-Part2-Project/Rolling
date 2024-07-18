8062943497954a44e6d64f2b2800664a7ddf06ba
김영주 <breadjoo1014@naver.com>
2024-07-18 12:20:39 +0900

    Design: Nav 적용, 마진 값 수정
    (cherry picked from commit 0ddcf0ab3bd8b545ec69a976ef3234386ec6bec7)

--- src/pages/postTheme/PostTheme.tsx
+++ src/pages/postTheme/PostTheme.tsx
@@ -3,6 +3,7 @@ import ThemeForm from "./components/ThemeForm";
 import ReceiverInput from "./components/ReceiverInput";
 import ThemeTypeSelection from "./components/ThemeTypeSelection";
 import { useThemeContext } from "./hooks/useThemeContext";
+import Nav from "../../components/Layout/Nav";
 
 const PostTheme: React.FC = () => {
   const {
@@ -15,21 +16,24 @@ const PostTheme: React.FC = () => {
   } = useThemeContext();
 
   return (
-    <main className="flex justify-center">
-      <ThemeForm
-        handleChange={handleChange}
-        themeData={themeData}
-        isButtonDisabled={isButtonDisabled}
-        handleButtonClick={handleButtonClick}
-        setThemeData={setThemeData}
-      >
-        <ReceiverInput handleChange={handleChange} themeData={themeData} />
-        <ThemeTypeSelection
-          setIsButtonDisabled={setIsButtonDisabled}
+    <>
+      <Nav />
+      <main className="flex justify-center">
+        <ThemeForm
+          handleChange={handleChange}
+          themeData={themeData}
+          isButtonDisabled={isButtonDisabled}
+          handleButtonClick={handleButtonClick}
           setThemeData={setThemeData}
-        />
-      </ThemeForm>
-    </main>
+        >
+          <ReceiverInput handleChange={handleChange} themeData={themeData} />
+          <ThemeTypeSelection
+            setIsButtonDisabled={setIsButtonDisabled}
+            setThemeData={setThemeData}
+          />
+        </ThemeForm>
+      </main>
+    </>
   );
 };
 
