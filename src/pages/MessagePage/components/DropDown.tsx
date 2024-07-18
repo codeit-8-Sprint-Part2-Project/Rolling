0f52f0c10e4e6e93942bf344e42954a2f3e16563
SealBros <hoon980923@gmail.com>
2024-07-18 23:33:51 +0900

    feat : dropdown 수정

--- src/pages/MessagePage/components/DropDown.tsx
+++ src/pages/MessagePage/components/DropDown.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export type DropdownOption = {
-    label: string;
-    value: string;
+  label: string;
+  value: string;
 };
 
 interface DropdownProps {
@@ -17,6 +17,23 @@ const Dropdown: React.FC<DropdownProps> = ({
   onOptionSelect,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -56,7 +73,8 @@ const Dropdown: React.FC<DropdownProps> = ({
 
       {isOpen && (
         <div
-          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dropdown-menu"
+          ref={dropdownRef}
+          className="absolute right-0 z-10 mt-2 w-80 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
