9e91ba4269be16b25834b027e3896105837ced15
김영주 <breadjoo1014@naver.com>
2024-07-18 17:39:37 +0900

    Design: 미리보기 모달창에 header, main 추가
    (cherry picked from commit 1795d405e80b4dc16a3a851744db0c7896b80e48)

--- src/pages/postTheme/components/ThemePreview.tsx
+++ src/pages/postTheme/components/ThemePreview.tsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
 import { ThemePreviewProps } from "../constants/propTypes";
 import { getColorClass } from "../utils/getColorClass";
+import PlusCard from "../../PostPage/components/PlusCard";
 
+// backgroundColorClass  themeData.backgroundImageURL
 interface ExtendedThemePreviewProps extends ThemePreviewProps {
   isThemeType: boolean;
 }
@@ -37,7 +39,7 @@ const ThemePreview: React.FC<ExtendedThemePreviewProps> = ({ themeData }) => {
         type="button"
         onClick={handleOpenModal}
         className={`absolute right-0 w-[140px] h-full max-md:w-[122px] rounded-sm ${
-          isDisabled ? "bg-gray-200" : "bg-violet-500 text-white"
+          isDisabled ? "bg-gray-248" : "bg-violet-548 text-white"
         }`}
         disabled={isDisabled}
       >
@@ -47,25 +49,39 @@ const ThemePreview: React.FC<ExtendedThemePreviewProps> = ({ themeData }) => {
         isOpen={isOpen}
         onRequestClose={handleCloseModal}
         contentLabel="미리보기 모달"
+        className="fixed inset-0 flex items-center justify-center"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-75"
       >
-        <div className="w-full h-full flex items-center justify-center rounded-lg">
-          <main
-            className={`w-full h-full ${backgroundColorClass}`}
-            style={{
-              backgroundImage: `url(${themeData.backgroundImageURL})`,
-              backgroundSize: "cover",
-            }}
+        <div className="flex flex-col bg-white rounded-lg overflow-auto max-h-[80vh] w-full max-w-5xl p-6 max-[1248px]:mx-6">
+          <div className="flex flex-col h-full">
+            <header className="h-[4.25rem] bg-white top-0 z-10 border-b border-solid border-[#EDEDED] flex-shrink-0">
+              <div className="h-full max-w-[78rem] mx-auto px-6 flex items-center">
+                <span className="text-[1.75rem] leading-[2.625rem] font-bold text-lg text-[#2B2B2B] grow">
+                  To. {themeData.name}
+                </span>
+              </div>
+            </header>
+            <main
+              className={`pb-60 bg-no-repeat bg-cover ${backgroundColorClass}`}
+              style={{
+                backgroundImage: `url(${themeData.backgroundImageURL})`,
+                backgroundSize: "cover",
+              }}
+            >
+              <section className="w-full pt-[7.0625rem] max-md:pt-8 pb-[2.375rem] bg-no-repeat bg-cover">
+                <div className="max-w-[40rem] mx-auto px-6 grid grid-cols-3 gap-x-6 gap-y-7 relative max-[1248px]:grid-cols-2 max-[1248px]:gap-4 max-md:grid-cols-1">
+                  <PlusCard />
+                </div>
+              </section>
+            </main>
+          </div>
+          <button
+            onClick={handleCloseModal}
+            className="w-full mt-4 p-2 bg-violet-548 text-white rounded"
           >
-            {/* 여기에 빈 post 페이지 스타일을 긁어옵니다.
-            x-iframe-option to deny */}
-          </main>
+            닫기
+          </button>
         </div>
-        <button
-          onClick={handleCloseModal}
-          className="w-full mt-4 p-2 bg-violet-500 text-white rounded"
-        >
-          닫기
-        </button>
       </Modal>
     </>
   );
