b3a6ad921fd1678a45906643226863e2322a3c42
SealBros <hoon980923@gmail.com>
2024-07-18 21:33:23 +0900

    feat: fontSelect 구현

--- src/pages/MessagePage/MessagePage.tsx
+++ src/pages/MessagePage/MessagePage.tsx
@@ -1,19 +1,32 @@
 import React, { useState } from "react";
 import InputProfileSection from "./components/InputProfileSection";
 import RelationshipSelectSection from "./components/RelationshipSelectSection";
+import FontSelectSection from "./components/FontSelectSection";
 
 type RelationShip = "친구" | "지인" | "동료" | "가족";
+type Font = "Noto Sans" | "Pretendard" | "나눔 면조" | "나눔손글씨" | "손편지체";
+
 
 const MessagePage: React.FC = () => {
+
+
   const [relationship, setRelationship] = useState<RelationShip>("지인");
+  
+  const handleRelationshipChange = (newRelationship: RelationShip) => {
+    setRelationship(newRelationship);
+  };
+
+  const [font, setFont] = useState<Font>("Noto Sans");
+  
+  const handleFontChange = (newFont: Font) => {
+    setFont(newFont);
+  };
 
   const handleImageUpload = (imageUrl: string) => {
     console.log("Uploaded image URL:", imageUrl);
   };
 
-  const handleRelationshipChange = (newRelationship: RelationShip) => {
-    setRelationship(newRelationship);
-  };
+  
 
   return (
     <div className="flex justify-center">
@@ -22,6 +35,10 @@ const MessagePage: React.FC = () => {
         <RelationshipSelectSection
           selectedRelationship={relationship}
           onRelationshipChange={handleRelationshipChange}
+        />
+        <FontSelectSection
+          selectedFont={font}
+          onFontChange={handleFontChange}
         />
       </div>
     </div>
