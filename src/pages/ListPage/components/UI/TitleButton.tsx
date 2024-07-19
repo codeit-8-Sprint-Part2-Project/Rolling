973f59d96db7de1ef7cc0b9f8a7efa76d7b85a73
DasomKim <ekt0521@naver.com>
2024-07-19 18:12:32 +0900

    Design: 나도 만들어보기 버튼 반응형 기능 추가

--- src/pages/ListPage/components/UI/TitleButton.tsx
+++ src/pages/ListPage/components/UI/TitleButton.tsx
@@ -7,9 +7,9 @@ interface TitleButtonProps {
 
 const TitleButton: React.FC<TitleButtonProps> = ({ children }) => {
   return (
-    <div className="container mx-auto max-w-[1200px] text-center mb-[150px] mt-[48px]">
-      <Link to="/post" className="inline-block">
-        <button className="px-[24px] py-[14px] w-[280px] border-none rounded-[12px] bg-[#9935ff] text-[18px] text-[#ffffff] font-pretendard">
+    <div className="container mx-auto max-w-[1200px] text-center mb-[150px]">
+      <Link to="/post">
+        <button className="px-6 py-[14px] w-full mt-12 border-none rounded-xl bg-[#9935ff] text-[18px] text-white font-pretendard md:w-[280px] fixed bottom-0 left-0 z-50 md:static md:relative md:m-0 m-[20px] max-w-[calc(100%-40px)]">
           {children}
         </button>
       </Link>
