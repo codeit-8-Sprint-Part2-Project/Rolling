0577be847c70ab10d53c15e423c32e469fc5d474
unknown <jingmsi014@gmail.com>
2024-07-21 16:51:52 +0900

    design: LazyLoading 요소에 애니메이션 요소 추가

--- src/pages/PostPage/components/LazyLoading.tsx
+++ src/pages/PostPage/components/LazyLoading.tsx
@@ -1,7 +1,11 @@
+import LoadingSpinner from "../../ListPage/components/UI/LoadingSpinner";
+
 function LazyLoading() {
 
     return (
-        <div className="bg-white/50 fixed inset-0 z-50"></div>
+        <div className="bg-white/50 flex justify-center items-center fixed inset-0 z-50">
+            <LoadingSpinner />
+        </div>
     )
 }
 
