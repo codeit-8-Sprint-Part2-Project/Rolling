6f1ae896c6fb4221d3cc09a5891f996b921e0841
DasomKim <ekt0521@naver.com>
2024-07-22 14:30:01 +0900

    Fix: massagecount 기능 수정

--- src/pages/ListPage/components/RecipientCard.tsx
+++ src/pages/ListPage/components/RecipientCard.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 interface RecipientCardProps {
     name: string;
+    messageCount: number;
     recentMessages: {
         id: number;
         sender: string;
@@ -14,6 +15,7 @@ interface RecipientCardProps {
 
 const RecipientCard: React.FC<RecipientCardProps> = ({
     name,
+    messageCount,
     recentMessages,
     topReactions,
     backgroundColor,
@@ -54,7 +56,7 @@ const RecipientCard: React.FC<RecipientCardProps> = ({
 
     return (
         <div
-            className={` absolute w-[275px] h-[260px] p-4 mb-4 border rounded-[16px] border-solid border-[#0000001A] shadow-md ${bgColorClass} relative overflow-hidden transition-all duration-300 ease-in-out`}
+            className={`absolute w-[275px] h-[260px] p-4 mb-4 border rounded-[16px] border-solid border-[#0000001A] shadow-md ${bgColorClass} relative overflow-hidden transition-all duration-300 ease-in-out`}
             style={{
                 ...cardStyle,
                 transform: isHovered ? 'scale(1.05)' : 'scale(1)',
@@ -63,81 +65,80 @@ const RecipientCard: React.FC<RecipientCardProps> = ({
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
-        {backgroundColor === "purple" && !backgroundImageURL && (<div
-        style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
-        className="absolute right-0 bottom-0"
-    >
-            <svg
-                width="142"
-                height="142"
-                viewBox="0 0 142 142"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="absolute right-0 bottom-0"
-            >
-                <g clipPath="url(#clip0_23083_1868)">
-                    <rect
-                        y="6"
-                        width="336"
-                        height="169"
-                        rx="84.5"
-                        fill="#DCB9FF"
-                        fillOpacity="0.4"
-                    />
-                </g>
-                <defs>
-                    <clipPath id="clip0_23083_1868">
-                        <rect width="142" height="142" fill="white" />
-                    </clipPath>
-                </defs>
-            </svg></div>
-        )}
-            {backgroundColor === "beige" && backgroundImageURL === null &&(
+            {backgroundColor === "purple" && !backgroundImageURL && (
                 <div
-        style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
-        className="absolute right-0 bottom-0"
-    >
-            <svg width="142" height="142" viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M21 57C21 28.8335 43.8335 6 72 6H302C330.167 6 353 28.8335 353 57V273C353 301.167 330.167 324 302 324H72C43.8335 324 21 301.167 21 273V57Z" fill="#FFD382" fill-opacity="0.7"/>
-</svg>
-</div>
+                    style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
+                    className="absolute right-0 bottom-0"
+                >
+                    <svg
+                        width="142"
+                        height="142"
+                        viewBox="0 0 142 142"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute right-0 bottom-0"
+                    >
+                        <g clipPath="url(#clip0_23083_1868)">
+                            <rect
+                                y="6"
+                                width="336"
+                                height="169"
+                                rx="84.5"
+                                fill="#DCB9FF"
+                                fillOpacity="0.4"
+                            />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_23083_1868">
+                                <rect width="142" height="142" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
             )}
-            {backgroundColor === "blue" && backgroundImageURL === null &&(
+            {backgroundColor === "beige" && backgroundImageURL === null && (
                 <div
-                style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
-                className="absolute right-0 bottom-0"
-            >
+                    style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
+                    className="absolute right-0 bottom-0"
+                >
                     <svg width="142" height="142" viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M74.4299 16.6978C88.1712 -5.00283 119.829 -5.00284 133.57 16.6978L202.482 125.526C217.239 148.829 200.495 179.25 172.912 179.25H35.0878C7.5049 179.25 -9.23877 148.829 5.51768 125.526L74.4299 16.6978Z" fill="#9DDDFF"/>
-</svg>
-
-        </div>
+                        <path d="M21 57C21 28.8335 43.8335 6 72 6H302C330.167 6 353 28.8335 353 57V273C353 301.167 330.167 324 302 324H72C43.8335 324 21 301.167 21 273V57Z" fill="#FFD382" fillOpacity="0.7" />
+                    </svg>
+                </div>
             )}
-            {backgroundColor === "green" && backgroundImageURL === null &&(
-                                <div
-                                style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
-                                className="absolute right-0 bottom-0"
-                            >
-                                    <svg width="142" height="142" viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g clip-path="url(#clip0_23083_4684)">
-<rect y="6" width="336" height="169" rx="84.5" fill="#9BE282" fill-opacity="0.3"/>
-</g>
-<defs>
-<clipPath id="clip0_23083_4684">
-<rect width="142" height="142" fill="white"/>
-</clipPath>
-</defs>
-</svg>
-
-                
-                        </div>
+            {backgroundColor === "blue" && backgroundImageURL === null && (
+                <div
+                    style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
+                    className="absolute right-0 bottom-0"
+                >
+                    <svg width="142" height="142" viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M74.4299 16.6978C88.1712 -5.00283 119.829 -5.00284 133.57 16.6978L202.482 125.526C217.239 148.829 200.495 179.25 172.912 179.25H35.0878C7.5049 179.25 -9.23877 148.829 5.51768 125.526L74.4299 16.6978Z" fill="#9DDDFF" />
+                    </svg>
+                </div>
+            )}
+            {backgroundColor === "green" && backgroundImageURL === null && (
+                <div
+                    style={{ width: '142px', height: '142px', overflow: 'hidden', borderBottomRightRadius: '16px' }}
+                    className="absolute right-0 bottom-0"
+                >
+                    <svg width="142" height="142" viewBox="0 0 142 142" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clipPath="url(#clip0_23083_4684)">
+                            <rect y="6" width="336" height="169" rx="84.5" fill="#9BE282" fillOpacity="0.3" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_23083_4684">
+                                <rect width="142" height="142" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
             )}
             <div className={`p-2 rounded-md relative flex flex-col justify-between h-full z-10 ${textColorClass} transition-opacity duration-300 ${isHovered ? 'opacity-100' : 'opacity-90'}`}>
                 <div>
                     <h2 className={`font-pretendard text-[24px] font-bold leading-[36px] tracking-[-0.01em] text-left`}>
                         To. {name}
                     </h2>
-                    {recentMessages.length === 0 && (
+                    {messageCount === 0 && (
                         <div className="mt-2">
                             <p className={`font-pretendard text-[16px] font-bold leading-[26px] tracking-[-0.01em] text-left inline`}>
                                 0
@@ -147,10 +148,10 @@ const RecipientCard: React.FC<RecipientCardProps> = ({
                             </span>
                         </div>
                     )}
-                    {recentMessages.length > 0 && (
+                    {messageCount > 0 && (
                         <div className="mt-2 relative">
                             <div className="flex flex-wrap gap-[-30px] h-7">
-                                {recentMessages.map((message, index) => (
+                                {recentMessages.slice(0, 3).map((message, index) => (
                                     <React.Fragment key={message.id}>
                                         <img
                                             src={message.profileImageURL}
@@ -162,49 +163,49 @@ const RecipientCard: React.FC<RecipientCardProps> = ({
                                                 zIndex: index,
                                             }}
                                         />
-                                        {index === recentMessages.length - 1 && (
-                                            <div
-                                                style={{
-                                                    position: "absolute",
-                                                    left: `${(index + 1) * 20}px`,
-                                                    zIndex: index + 1,
-                                                }}
-                                            >
-                                                <div
-                                                    style={{
-                                                        width: "33px", 
-                                                        height: "28px", 
-                                                        backgroundColor: "white",
-                                                        borderRadius: "50%",
-                                                        display: "flex",
-                                                        alignItems: "center",
-                                                        justifyContent: "center",
-                                                    }}
-                                                >
-                                                    <p
-                                                        style={{
-                                                            fontFamily: "Pretendard",
-                                                            fontSize: "12px",
-                                                            fontWeight: 400,
-                                                            lineHeight: "18px",
-                                                            letterSpacing: "-0.005em",
-                                                            textAlign: "left",
-                                                            color: "rgba(85, 85, 85, 1)",
-                                                            margin: 0,
-                                                        }}
-                                                    >
-                                                        <span>+</span>
-                                                        {recentMessages.length}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        )}
                                     </React.Fragment>
                                 ))}
+                                {(messageCount - 3) > 0 && (
+                                    <div
+                                        style={{
+                                            position: "absolute",
+                                            left: `${3 * 20}px`,
+                                            zIndex: 3,
+                                        }}
+                                    >
+                                        <div
+                                            style={{
+                                                width: "33px",
+                                                height: "28px",
+                                                backgroundColor: "white",
+                                                borderRadius: "50%",
+                                                display: "flex",
+                                                alignItems: "center",
+                                                justifyContent: "center",
+                                            }}
+                                        >
+                                            <p
+                                                style={{
+                                                    fontFamily: "Pretendard",
+                                                    fontSize: "12px",
+                                                    fontWeight: 400,
+                                                    lineHeight: "18px",
+                                                    letterSpacing: "-0.005em",
+                                                    textAlign: "left",
+                                                    color: "rgba(85, 85, 85, 1)",
+                                                    margin: 0,
+                                                }}
+                                            >
+                                                <span>+</span>
+                                                {messageCount - 3}
+                                            </p>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
                             <div className="mt-2">
                                 <p className={`font-pretendard text-[16px] font-bold leading-[26px] tracking-[-0.01em] text-left inline`}>
-                                    {recentMessages.length}
+                                    {messageCount}
                                 </p>
                                 <span className={`font-pretendard text-[16px] font-normal leading-[26px] tracking-[-0.01em] text-left inline`}>
                                     명이 작성했어요!
