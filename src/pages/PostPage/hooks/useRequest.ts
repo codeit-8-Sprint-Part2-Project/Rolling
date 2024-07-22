f4496d8b2b00c1b1c67cc31105cf8fc88ff0bd75
unknown <jingmsi014@gmail.com>
2024-07-22 21:58:18 +0900

    fix: useRequest의 리턴에 await을 붙임으로써 isPending이 작동하지 않던 오류 수정

--- src/pages/PostPage/hooks/useRequest.ts
+++ src/pages/PostPage/hooks/useRequest.ts
@@ -6,7 +6,7 @@ function useRequest() {
     const wrappedRequest = useCallback (async (method: any, id: number | string, limit?: number, offset?: number) => {
         try {
             setIsPending(true);
-            return (limit && offset) ? method(id, limit, offset) : method(id);
+            return (limit && offset) ? await method(id, limit, offset) : await method(id);
         } catch(e: any) {
             alert(`useRequest에서 출력하는 오류: ${e.message}`);
         } finally {
