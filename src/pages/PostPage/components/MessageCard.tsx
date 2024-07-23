2ac01cc0765a485591a6254cc60ad55bc00214f4
unknown <jingmsi014@gmail.com>
2024-07-23 15:40:07 +0900

    design: 모바일 화면에서 메세지 제목이 길 경우 모달의 너비가 의도보다 커지는 현상 수정

--- src/pages/PostPage/components/MessageCard.tsx
+++ src/pages/PostPage/components/MessageCard.tsx
@@ -69,7 +69,7 @@ function MessageCard({ message = INITIAL_MESSAGE_VALUE, isEditing = false, handl
     return (
         <>
             <div className={"CARD h-[17.5rem] rounded-2xl bg-white pt-7 px-6 pb-6 flex flex-col cursor-pointer shadow-[0_2px_12px_0_rgba(0,0,0,0.08)] max-[1200px]:h-[17.75rem] max-md:h-[14.375rem] hover:scale-105 transition-all duration-300 ease-in-out"} onClick={handleCardClick}>
-                <div className="pb-4 border-solid border-b border-[#EEEEEE] flex justify-between truncate">
+                <div className="pb-4 border-solid border-b border-[#EEEEEE] flex justify-between">
                     <SenderInfo message={messageState} />
                     {isEditing && <TrashcanButton onClick={handleTrashcanClick} />}
                 </div>
